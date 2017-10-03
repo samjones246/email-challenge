7c6efc7f25bd1ef56f518401da82785e9fbc767d
samjones246 <32492579+samjones246@users.noreply.github.com>
2017-10-03 19:04:13 +0100

    Add files via upload

---
+++ EmailChallenge.java
@@ -0,0 +1,26 @@
+package emailchallenge;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.URL;
+public class EmailChallenge {
+    public static void main(String[] args) throws Exception{
+        System.out.print("Enter email ID to look up: ");
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String id = br.readLine();
+        URL address = new URL("http://www.ecs.soton.ac.uk/people/" + id);
+        BufferedReader urlReader = new BufferedReader(new InputStreamReader(address.openStream()));
+        String line, name ="";
+        while ((line=urlReader.readLine())!=null && name.equals("")){
+            if (line.contains("<title>")){
+                name = line;
+            }
+        }
+        name = name.substring(name.indexOf(">")+1, name.indexOf("|"));
+        if (name.equals("People ")){
+            System.out.println("Invalid ID");
+        }else{
+            System.out.println(name);
+        }
+    }
+    
+}
